e9564375654d1440d4b0720dd76dd8594a91ba8b
Shanu30 <ksechenari@gmail.com>
2022-03-14 03:44:28 +0530

    portfolio

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export { default as About } from "./About";
+export { default as Blog } from "./Blog";
+export { default as Intro } from "./Intro";
+export { default as Header } from "./Header";
+export { default as Contact } from "./Contact";
+export { default as Projects } from "./Projects";
+export { default as Skills } from "./Skills";
